2e1f835aa8fad9b4a84e81d9a8f063e8e490f5d8
Nicolas <nicolas.delhomme@umu.se>
2020-03-23 16:30:38 +0100

    Extended the README. Revisited the report to make it pretty and add the Line comparison at T0 and T1.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # poplarCambium
+
+## Setup
+```{bash}
+ln -s /mnt/picea/projects/aspseq/cbellini/24_Bellini_cambium data
+```
--- src/R/DifferentialExpression_2020.R
+++ src/R/DifferentialExpression_2020.R
@@ -10,28 +10,29 @@
 #' # Setup
 
 #' * Libraries
-suppressPackageStartupMessages(library(data.table))
-suppressPackageStartupMessages(library(DESeq2))
-suppressPackageStartupMessages(library(gplots))
-suppressPackageStartupMessages(library(here))
-suppressPackageStartupMessages(library(hyperSpec))
-suppressPackageStartupMessages(library(RColorBrewer))
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(VennDiagram))
+suppressPackageStartupMessages({
+    library(data.table)
+    library(DESeq2)
+    library(gplots)
+    library(here)
+    library(hyperSpec)
+    library(RColorBrewer)
+    library(tidyverse)
+    library(VennDiagram)
+})
 
 #' * Helper files
-suppressMessages(source(here("UPSCb-common/src/R/featureSelection.R")))
-suppressMessages(source(here("UPSCb-common/src/R/plotMA.R")))
-suppressMessages(source(here("UPSCb-common/src/R/volcanoPlot.R")))
-suppressMessages(source(here("UPSCb-common/src/R/gopher.R")))
+suppressMessages({
+    source(here("UPSCb-common/src/R/featureSelection.R"))
+    source(here("UPSCb-common/src/R/plotMA.R"))
+    source(here("UPSCb-common/src/R/volcanoPlot.R"))
+    source(here("UPSCb-common/src/R/gopher.R"))
+})
 
 #' * Graphics
 pal=brewer.pal(8,"Dark2")
 hpal <- colorRampPalette(c("blue","white","red"))(100)
 mar <- par("mar")
-
-#' TODO remember to add a function to check for correlation between logFC and 
-#' transcript length - check for the effective length
 
 #' * Functions
 #' 1. plot specific gene expression
@@ -67,7 +68,7 @@ mar <- par("mar")
                               labels=colnames(dds),
                               sample_sel=1:ncol(dds),
                               expression_cutoff=0,
-                              debug=FALSE){
+                              debug=FALSE,...){
     
     if(length(contrast)==1){
         res <- results(dds,name=contrast)
@@ -110,14 +111,14 @@ mar <- par("mar")
                                   ! is.na(res$padj) & res$baseMean >= qe
                           }),sum))
         
-        plot(ggplot(dat,aes(x=thetas,y=qtl.exp)) + 
-                 geom_line() + geom_point() +
-                 scale_x_continuous("quantiles of expression") + 
-                 scale_y_continuous("base mean expression") +
-                 geom_hline(yintercept=expression_cutoff,
-                            linetype="dotted",col="red"))
-        
         if(debug){
+            plot(ggplot(dat,aes(x=thetas,y=qtl.exp)) + 
+                     geom_line() + geom_point() +
+                     scale_x_continuous("quantiles of expression") + 
+                     scale_y_continuous("base mean expression") +
+                     geom_hline(yintercept=expression_cutoff,
+                                linetype="dotted",col="red"))
+            
             p <- ggplot(dat,aes(x=thetas,y=qtl.exp)) + 
                 geom_line() + geom_point() +
                 scale_x_continuous("quantiles of expression") + 
@@ -148,15 +149,16 @@ mar <- par("mar")
                      geom_line() + geom_point() +
                      scale_x_continuous("base mean of expression") + 
                      scale_y_continuous("Number of DE genes per interval"))
+        
+            p <- ggplot(data.frame(x=dat$qtl.exp[-1],
+                                   y=diff(dat$number.degs[1] - dat$number.degs)),aes(x,y)) + 
+                geom_line() + geom_point() +
+                scale_x_log10("base mean of expression") + 
+                scale_y_continuous("Number of DE genes per interval") + 
+                geom_vline(xintercept=expression_cutoff,
+                           linetype="dotted",col="red")
+            suppressMessages(suppressWarnings(plot(p)))
         }
-        p <- ggplot(data.frame(x=dat$qtl.exp[-1],
-                               y=diff(dat$number.degs[1] - dat$number.degs)),aes(x,y)) + 
-            geom_line() + geom_point() +
-            scale_x_log10("base mean of expression") + 
-            scale_y_continuous("Number of DE genes per interval") + 
-            geom_vline(xintercept=expression_cutoff,
-                       linetype="dotted",col="red")
-        suppressMessages(suppressWarnings(plot(p)))
     }
     
     sel <- res$padj <= padj & abs(res$log2FoldChange) >= lfc & ! is.na(res$padj) & 
@@ -186,7 +188,7 @@ mar <- par("mar")
                   distfun = pearson.dist,
                   hclustfun = function(X){hclust(X,method="ward.D2")},
                   trace="none",col=hpal,labRow = FALSE,
-                  labCol=labels[sample_sel]
+                  labCol=labels[sample_sel],...
         )
     }
     return(list(all=rownames(res[sel,]),
@@ -201,6 +203,7 @@ mar <- par("mar")
 #' biological QA template, you need not change anything
 #' ```
 load(here("data/analysis/DE-new/DESeqDataset-20170703.rda"))
+dds$LineTimeSample <- sub("\\.1$","",dds$LineTimeSample)
 
 #' ## Normalisation for visualisation
 vsd <- varianceStabilizingTransformation(dds,blind=FALSE)
@@ -208,6 +211,8 @@ vst <- assay(vsd)
 vst <- vst - min(vst)
 dir.create(here("data/analysis/DE-new"),showWarnings=FALSE)
 save(vst,file=here("data/analysis/DE-new/vst-aware.rda"))
+write_csv(vst %>% as.data.frame %>% rownames_to_column("ID"),
+          path=here("data/analysis/DE-new/vst-aware.csv"))
 
 #' ## Differential Expression
 dds <- DESeq(dds)
@@ -220,114 +225,72 @@ plotDispEsts(dds)
 resultsNames(dds)
 
 #' ## Results
-#' #' ```{r res, echo=FALSE,eval=FALSE}
-#' CHANGEME - here you need to define the contrast you want to 
-#' study - see the example in the next block. 
-#' 
-#' The `contrast` can be given
-#' by name, as a list (numerator/denominator) or as a vector of weight (e.g. c(0,1));
-#' read the DESeq2 vignette for more info
-#' 
-#' The `label` argument is typically one (or a combination) of the metadata stored in colData
-#' 
-#' The function allows for looking at the independent filtering results using `debug=TRUE`
-#' 
-#' If you are not satisfied with the default from DESeq2, you can set your own cutoff using `expression_cutoff`
-#' 
-#' You can change the default output file prefix using `default_prefix`
-#' 
-#' You can select the set of samples to be added to the `heatmap`, using the `sample_sel` argument. It takes a logical vector.
-#' 
-#' ```
-
-#' ```{r contrast, echo=FALSE,eval=FALSE}
-#'contrast1 <- extract_results(dds=dds,vst=vst,contrast="CHANGEME")
-#' ```
+#' ### T89 T1 vs. T0
 contrast_T89 <- extract_results(dds=dds,vst=vst,contrast=c(0,0,1,1),
                               expression_cutoff=10,
-                              default_prefix="Time_T1_vs_T0",
-                              sample_sel=dds$Time %in% c("T0","T1") & dds$Line == "T89") 
+                              default_prefix="T89-Time_T1_vs_T0",
+                              labels=dds$LineTimeSample,
+                              sample_sel=dds$Time %in% c("T0","T1") & dds$Line == "T89",
+                              cexCol=0.8) 
 
-#' Extract results of GATA expirement, G+ vs G-
+#' ### OP42 T1 vs. T0
 contrast_OP42 <- extract_results(dds=dds,vst=vst,contrast="Time_T1_vs_T0",
-                              expression_cutoff=10,
-                              default_prefix="Time_T1_vs_T0",
-                              sample_sel=dds$Time %in% c("T0","T1") & dds$Line == "OP42") 
+                              default_prefix="OP42-Time_T1_vs_T0",
+                              labels=dds$LineTimeSample,
+                              sample_sel=dds$Time %in% c("T0","T1") & dds$Line == "OP42",
+                              cexCol=0.8) 
+
+#' ### T89 vs. OP42 at T0
+contrast_T0 <- extract_results(dds=dds,vst=vst,contrast="Line_T89_vs_OP42",
+                               default_prefix="T0-T89_vs_OP42",
+                               labels=dds$LineTimeSample, 
+                               sample_sel=dds$Time == "T0",
+                               cexCol=0.8) 
+
+#' ### T89 vs. OP42 at T1
+contrast_T1 <- extract_results(dds=dds,vst=vst,contrast=c(0,1,0,1),
+                               default_prefix="T1-T89_vs_OP42",
+                               labels=dds$LineTimeSample,
+                               sample_sel=dds$Time == "T1",
+                               cexCol=0.8)
 
 #' ### Venn Diagram
-#' ```{r venn, echo=FALSE,eval=FALSE}
-#' CHANGEME - Here, you typically would have run several contrasts and you want
-#' to assess their overlap plotting VennDiagrams.
+#' Abbreviations:
 #' 
-#' In the examples below, we assume that these resutls have been saved in a list
-#' called `res.list`
-#' ```
+#' * OP42: comparison T1 vs. T0 for OP42
+#' 
+#' * T89: comparison T1 vs. T0 for OP42
+#' 
+#' * T0: comparison T89 vs. OP42 at T0
+#' 
+#' * T1: comparison T89 vs. OP42 at T1
+#' 
 res.list <- list(OP42=contrast_OP42,
-                 T89=contrast_T89)
+                 T1=contrast_T1,
+                 T89=contrast_T89,
+                 T0=contrast_T0)
 
 
 #' #### All DE genes
-#' ```{r venn2, echo=FALSE,eval=FALSE}
 grid.newpage()
 grid.draw(venn.diagram(lapply(res.list,"[[","all"),
                        NULL,
-                       fill=pal[1:2]))
-#' ```
+                       fill=pal[1:4]))
 
-#' #### DE genes (up in mutant)
-#' ```{r venn3, echo=FALSE,eval=FALSE}
+#' #### DE genes (up)
 grid.newpage()
 grid.draw(venn.diagram(lapply(res.list,"[[","up"),
                       NULL,
-                      fill=pal[1:2]))
-#' ```
+                      fill=pal[1:4]))
 
-#' #### DE genes (up in control)
-#' ```{r venn4, echo=FALSE,eval=FALSE}
+#' #### DE genes (down)
 grid.newpage()
 grid.draw(venn.diagram(lapply(res.list,"[[","dn"),
                        NULL,
-                       fill=pal[1:2]))
-#' ```
+                       fill=pal[1:4]))
 
-#' ### Gene Ontology enrichment
-#' ```{r go, echo=FALSE,eval=FALSE}
-#' Once you have obtained a list of candidate genes, you most probably want
-#' to annotate them.
-#' 
-#' In the following example, we first identify the background; _i.e._ the
-#' population of expressed genes. We select the genes expressed in a least
-#' 2 replicate of one condition at a cutoff of `exp`.
-#' 
-#' Next we run the enrichment, in the example against `athaliana` using 
-#' the gofer3 REST API (interfaced through the gopher.R script loaded at the
-#' beginning of this fil).
-#' 
-#' Finally we export the go enrichment as a complete table and as a table consisting
-#' of only the `id` and `padj` columns. The latter can be used as input for _e.g._
-#' REVIGO.
-#' ```
-#background <- rownames(vst)[featureSelect(vst,dds$MGenotype,exp=CHANGEME)]
-
-#enr.list <- lapply(res.list,function(r){
-#    lapply(r,gopher,background=background,task="go",url="athaliana")
-#})
-
-#dev.null <- lapply(names(enr.list),function(n){
-#    r <- enr.list[[n]]
-#    write_delim(r$all$go,path=file.path(file.path(here("data/analysis/DE",
-#                                              paste0(n,"-all-DE-genes_GO-enrichment.txt")))))
-#    write_delim(r$all$go[,c("id","padj")],path=file.path(file.path(here("data/analysis/DE",
-#                                                       paste0(n,"-all-DE-genes_GO-enrichment_for-REVIGO.txt")))))
-#    write_csv(r$up$go,path=file.path(file.path(here("data/analysis/DE",
-#                                                 paste0(n,"-up-DE-genes_GO-enrichment.txt")))))
-#    write_delim(r$up$go[,c("id","padj")],path=file.path(file.path(here("data/analysis/DE",
-#                                                                        paste0(n,"-up-DE-genes_GO-enrichment_for-REVIGO.txt")))))    
-#    write_csv(r$dn$go,path=file.path(file.path(here("data/analysis/DE",
-#                                                 paste0(n,"-down-DE-genes_GO-enrichment.txt")))))
-#    write_delim(r$dn$go[,c("id","padj")],path=file.path(file.path(here("data/analysis/DE",
-#                                                                        paste0(n,"-down-DE-genes_GO-enrichment_for-REVIGO.txt")))))    
-#})
+#' ### Barplot
+barplot(sapply(lapply(res.list,"[",-1),function(r){sapply(r,length)}) * c(1,-1),beside=TRUE)
 
 #' # Session Info 
 #'  ```{r session info, echo=FALSE}
